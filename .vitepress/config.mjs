5fccbee2fc605d3aea228116ab2ac7acf5faf68c
sonaam⚡ <sonaam.hitang@gmail.com>
2025-04-20 12:27:46 +0545

    ci/cd: fix deployy

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/rooms-sdk-documentation/',
   title: "ST Rooms SDK",
   description: "Documentation",
   themeConfig: {
